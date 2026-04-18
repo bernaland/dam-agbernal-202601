0726938b1fa6d0f84e099d9eb4336ba6b8ef5caa
Andres Bernal <agbernala@hotmail.com>
2026-04-17 19:10:34 -0500

    Add KeyPad organism. Add barrel file to export it

---
+++ usandoHooks/src/components/organisms/calculator/KeyPad.style.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+import { colors, space } from "../../../Themes";
+
+export const stylesKeyPad = StyleSheet.create({
+  buttonRow: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+    padding: space.m,
+  },
+  container: {
+    flexDirection: "column",
+    flex: 1,
+    justifyContent: "space-between",
+    padding: space.m,
+  }
+});
